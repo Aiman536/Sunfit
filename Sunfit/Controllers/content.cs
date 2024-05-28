e9d3da39254b05af2571591fb29f5fb986a4a9aa
LENOVO <LENOVO@DESKTOP-L0LF1JF>
2024-05-28 12:06:35 -0700

    Add project files.

---
+++ Sunfit/Controllers/content.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sunfit.Controllers
+{
+    public class content : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
